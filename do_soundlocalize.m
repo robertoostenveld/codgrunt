6a614e0122fb1d2140f3747c52ffa6f5dc95cc4e
Robert Oostenveld <r.oostenveld@gmail.com>
2021-12-12 17:14:55 +0100

    use pos instead of elecpos, ft_plot_sens will still work

--- do_soundlocalize.m
+++ do_soundlocalize.m
@@ -1,7 +1,7 @@
 %%
 
 cube = [];
-cube.elecpos = [
+cube.pos = [
   0 0 0
   1 0 0
   1 1 0
@@ -20,7 +20,7 @@ ft_plot_axes(cube, 'fontsize', eps) % the axes are approx "head-sized" and 15cm 
 %%
 
 tetraheder = [];
-tetraheder.elecpos = [
+tetraheder.pos = [
   0 0 0
   1 1 0
   1 0 1
@@ -35,12 +35,14 @@ ft_plot_axes(tetraheder, 'fontsize', eps) % the axes are approx "head-sized" and
 %%
 
 hydrophone = tetraheder;
-hydrophone.elecpos(:,1) = hydrophone.elecpos(:,1) - mean(hydrophone.elecpos(:,1));
-hydrophone.elecpos(:,2) = hydrophone.elecpos(:,2) - mean(hydrophone.elecpos(:,2));
-hydrophone.elecpos(:,3) = hydrophone.elecpos(:,3) - mean(hydrophone.elecpos(:,3));
+hydrophone.pos(:,1) = hydrophone.pos(:,1) - mean(hydrophone.pos(:,1));
+hydrophone.pos(:,2) = hydrophone.pos(:,2) - mean(hydrophone.pos(:,2));
+hydrophone.pos(:,3) = hydrophone.pos(:,3) - mean(hydrophone.pos(:,3));
 
 % the radius of the sphere connecting the centres of the hydrophones was approximately 173 mm
-hydrophone.elecpos = 2*0.173 * hydrophone.elecpos/sqrt(3/4);
+hydrophone.pos = 0.173 * hydrophone.pos/sqrt(3/4);
+
+fprintf('The distance between microphones is %f %s\n', norm(hydrophone.pos(1,:)-hydrophone.pos(2,:)), hydrophone.unit)
 
 ft_plot_sens(hydrophone, 'elecshape', 'sphere', 'elecsize', 0.03, 'label', 'number')
 ft_plot_axes(hydrophone, 'fontsize', eps) % the axes are approx "head-sized" and 15cm long
@@ -49,32 +51,31 @@ ft_plot_axes(hydrophone, 'fontsize', eps) % the axes are approx "head-sized" and
 
 clear toa tdoa
 
-% speed = 343; % m/s
-speed = 1500; % m/s
+speed = 343; % m/s
+% speed = 1500; % m/s
 
 noiselevel = 1e-6; % in seconds, note that 2e-5 corresponds to one sample at 48000
 
-source = [1 0 0];
+soundpos = [1 0 0];
 for i=1:length(hydrophone.label)
-  toa(i,1) = norm(hydrophone.elecpos(i,:)-source)/speed; % in seconds
+  toa(i,1) = norm(hydrophone.pos(i,:)-soundpos)/speed; % in seconds
 end
 
 ft_plot_sens(hydrophone, 'elecshape', 'sphere', 'elecsize', 0.03, 'label', 'number')
 ft_plot_axes(hydrophone, 'fontsize', eps) % the axes are approx "head-sized" and 15cm long
-ft_plot_dipole(source, [0 0 +1], 'diameter', 0.03, 'length', 0.03, 'color', 'k');
+ft_plot_dipole(soundpos, [0 0 +1], 'diameter', 0.03, 'length', 0.03, 'color', 'k');
 
 for i=1:10
   tdoa = toa-min(toa); % in seconds
   tdoa = tdoa + noiselevel * randn(size(tdoa));
   
-  [eX,eb] = algebraicGPSequations(hydrophone.elecpos, tdoa*speed);
+  [eX,eb] = algebraicGPSequations(hydrophone.pos, tdoa*speed);
   
   ft_plot_dipole(eX, [0 0 -1], 'diameter', 0.03, 'length', 0.03, 'color', 'm');
 end
 
 axis on
 axis equal
-grid on 
+grid on
 axis vis3d
 view(30, 30)
-
